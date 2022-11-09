92a67b3de9bd3002ba8fabf88803ec18f1f3b031
Moa Junior <moacirfonsecajr@gmail.com>
2022-11-09 15:55:52 -0300

    Implementa pagina de busca inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    listItems: [],
+  };
+
+  render() {
+    const { listItems } = this.state;
+    return (
+      <div>
+        <input type="text" />
+        {
+          listItems.length !== 0 ? null : (
+            <p data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
