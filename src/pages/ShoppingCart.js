da237395effa1e67900773eff0024e5d78be6399
Bruno Ferreira <contatobrunokye@gmail.com>
2022-11-10 19:07:23 -0300

    Adiciona botao remover

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 
 export default class ShoppingCart extends Component {
-  render() {
+  state = {
+    cart: [],
+  };
+
+  componentDidMount() {
+    this.handleLocalStorage();
+  }
+
+  handleLocalStorage = () => {
     const cart = JSON.parse(localStorage.getItem('cart'));
+    this.setState({ cart });
+  };
+
+  handleDelete = ({ target }) => {
+    const { name } = target;
+    const getItems = JSON.parse(localStorage.getItem('cart'));
+    const filteredItems = getItems.filter((product) => product.title !== name);
+
+    localStorage.setItem('cart', JSON.stringify(filteredItems));
+    this.handleLocalStorage();
+  };
+
+  render() {
+    const { cart } = this.state;
+
     return (
       <div data-testid="shopping-cart-empty-message">
         {
-          cart
+          cart.length > 0
             ? cart.map((item) => (
               <div key={ item.id }>
+                <button
+                  data-testid="remove-product"
+                  type="button"
+                  name={ item.title }
+                  onClick={ this.handleDelete }
+                >
+                  Remover
+                </button>
                 <img src={ item.thumbnail } alt={ item.title } />
-                <h1 data-testid="shopping-cart-product-name">{item.title}</h1>
-                <h2>{item.price}</h2>
-                <h3 data-testid="shopping-cart-product-quantity">{item.quantity}</h3>
+                <p data-testid="shopping-cart-product-name">{item.title}</p>
+                <p>{item.price}</p>
+                <p data-testid="shopping-cart-product-quantity">{item.quantity}</p>
               </div>
             ))
             : 'Seu carrinho está vazio'
