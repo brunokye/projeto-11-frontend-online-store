a7d7349a6c270af4c7853ee6c5e09863c67a56ec
Moa Junior <moacirfonsecajr@gmail.com>
2022-11-10 21:05:17 -0300

    implementa carrinho de comrpas

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -24,31 +24,75 @@ export default class ShoppingCart extends Component {
     this.handleLocalStorage();
   };
 
+  handleDecrement = ({ target }) => {
+    const { id } = target;
+    const { cart } = this.state;
+    cart.filter((item) => {
+      if (item.id === id && item.quantity > 1) {
+        item.quantity -= 1;
+      }
+      return item;
+    });
+    this.setState({ cart });
+  };
+
+  handleIncrement = ({ target }) => {
+    const { id } = target;
+    const { cart } = this.state;
+    cart.filter((item) => {
+      if (item.id === id) {
+        item.quantity += 1;
+      }
+      return item;
+    });
+    this.setState({ cart });
+  };
+
   render() {
     const { cart } = this.state;
 
     return (
       <div data-testid="shopping-cart-empty-message">
-        {
-          cart
-            ? cart.map((item) => (
-              <div key={ item.id } className="product-container">
-                <button
-                  data-testid="remove-product"
-                  type="button"
-                  name={ item.title }
-                  onClick={ this.handleDelete }
-                >
-                  Remover
-                </button>
-                <img src={ item.thumbnail } alt={ item.title } />
-                <p data-testid="shopping-cart-product-name">{item.title}</p>
-                <p>{`R$ ${item.price}`}</p>
-                <p data-testid="shopping-cart-product-quantity">{item.quantity}</p>
-              </div>
-            ))
-            : 'Seu carrinho está vazio'
-        }
+        {cart
+          ? cart.map((item) => (
+            <div
+              key={ item.id }
+              className="product-container"
+            >
+              <button
+                data-testid="remove-product"
+                type="button"
+                name={ item.title }
+                onClick={ this.handleDelete }
+              >
+                Remover
+              </button>
+              <img
+                src={ item.thumbnail }
+                alt={ item.title }
+              />
+              <p data-testid="shopping-cart-product-name">{item.title}</p>
+              <p>{`R$ ${item.price}`}</p>
+              <button
+                data-testid="product-decrease-quantity"
+                type="button"
+                id={ item.id }
+                onClick={ this.handleDecrement }
+              >
+                -
+              </button>
+              <p data-testid="shopping-cart-product-quantity">{item.quantity}</p>
+              <button
+                data-testid="product-increase-quantity"
+                type="button"
+                id={ item.id }
+                onClick={ this.handleIncrement }
+              >
+                +
+              </button>
+            </div>
+          ))
+          : 'Seu carrinho está vazio'}
       </div>
     );
   }
